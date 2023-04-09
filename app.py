d029fc06d0fffb533e4b33b2032b2455ab8bc6f9
Ankit Raj <96245152+Ankiit04@users.noreply.github.com>
2023-04-09 14:57:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
 import signal
-import threading
-from flask_socketio import SocketIO   
-app = Flask(__name__)
-socketio = SocketIO(app)
 
 # Load the IPL matches dataset
 ipl = pd.read_csv('matches.csv')
@@ -16,118 +13,61 @@ ipl = pd.read_csv('matches.csv')
 team_rename_dict = {
     'Gujarat Lions': 'Gujarat Titans',
     'Kings XI Punjab': 'Punjab Kings',
-     'Rising Pune Supergiant': 'Lucknow Supergiants',
-    'Delhi Daredevils': 'Delhi Capitals',
-    
-    # Add more team name mappings as needed
+    'Rising Pune Supergiant': 'Lucknow Supergiants',
+    'Delhi Daredevils': 'Delhi Capitals'
 }
 
+# Replace team names in the dataset using the team_rename_dict
+ipl.replace({'team1': team_rename_dict, 'team2': team_rename_dict, 'winner': team_rename_dict}, inplace=True)
 
-# Define dictionary to rename venue names
-venue_rename_dict = {
-    'ACA-VDCA Stadium': 'Arun Jaitley Stadium',
-    'Green Park': 'Narendra MOdi Stadium',
-    'Buffalo Park' :'Ekana Cricket Stadium'
+# Label encode the categorical features
+le = LabelEncoder()
+ipl['team1'] = le.fit_transform(ipl['team1'])
+ipl['team2'] = le.transform(ipl['team2'])
+ipl['toss_winner'] = le.transform(ipl['toss_winner'])
+ipl['venue'] = le.transform(ipl['venue'])
+ipl['winner'] = le.transform(ipl['winner'])
 
-    # Add more venue name mappings as needed
-}
+# Define X and y for training
+X = ipl[['team1', 'team2', 'toss_winner', 'venue']]
+y = ipl['winner']
 
-# Rename team names
-ipl['team1'] = ipl['team1'].replace(team_rename_dict)
-ipl['team2'] = ipl['team2'].replace(team_rename_dict)
-ipl['winner'] = ipl['winner'].replace(team_rename_dict)
+# Initialize the Decision Tree Classifier
+dtc = DecisionTreeClassifier()
 
-# Rename venue names
-ipl['venue'] = ipl['venue'].replace(venue_rename_dict)
+# Fit the model
+dtc.fit(X, y)
 
-
-
-
-
-# Drop unnecessary columns
-ipl.drop(['dl_applied', 'toss_winner', 'toss_decision', 'result', 'win_by_runs', 'win_by_wickets', 'player_of_match', 'umpire1', 'umpire2', 'umpire3', 'date', 'city', 'season', 'id'], axis=1, inplace=True)
-
-# Drop rows with NaN values in the 'winner' column
-ipl.dropna(subset=['winner'], inplace=True)
-
-# Encode categorical variables
-team_label_encoder = LabelEncoder()
-venue_label_encoder = LabelEncoder()
-ipl['team1'] = team_label_encoder.fit_transform(ipl['team1'])
-ipl['team2'] = team_label_encoder.transform(ipl['team2'])
-ipl['winner'] = team_label_encoder.transform(ipl['winner'])
-ipl['venue'] = venue_label_encoder.fit_transform(ipl['venue'])
-
-teams_to_remove = ['Kochi Tuskers Kerala', 'Pune Warriors','Deccan Chargers']
-
-# Filter out matches involving teams to be removed
-ipl = ipl[~ipl['team1'].isin(teams_to_remove)]
-ipl = ipl[~ipl['team2'].isin(teams_to_remove)]
-ipl = ipl[~ipl['winner'].isin(teams_to_remove)]
-
-# Get unique team names and venue names
-teams = [team for team in team_label_encoder.inverse_transform(ipl['team1'].unique())]
-teams.remove(teams_to_remove[0])
-teams.remove(teams_to_remove[1])
-teams.remove(teams_to_remove[2])
-
-
-venues = venue_label_encoder.inverse_transform(ipl['venue'].unique())
-
+# Define signal handler to handle SIGINT (Ctrl+C)
 def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    exit(0)
 
+# Register Flask routes
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+socketio = SocketIO(app)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    # Split the data into training and testing sets
-    X_train = ipl[['team1', 'team2', 'venue']]
-    y_train = ipl['winner']
+@app.route('/predict', methods=['POST'])
+def predict():
+    team1 = le.transform([request.form['team1']])[0]
+    team2 = le.transform([request.form['team2']])[0]
+    toss_winner = le.transform([request.form['toss_winner']])[0]
+    venue = le.transform([request.form['venue']])[0]
+    features = np.array([team1, team2, toss_winner, venue]).reshape(1, -1)
+    prediction = dtc.predict(features)
+    predicted_winner = le.inverse_transform(prediction)[0]
+    return render_template('index.html', prediction=predicted_winner)
 
-    model = DecisionTreeClassifier()
-    model.fit(X_train, y_train)
+@app.route('/tipme')
+def tipme():
+    return render_template('tipme.html')
 
-    @app.route('/')
-    def form():
-        # Get unique team names and venue names
-        teams = team_label_encoder.inverse_transform(ipl['team1'].unique())
-        venues = venue_label_encoder.inverse_transform(ipl['venue'].unique())
-        return render_template('form.html', teams=teams, venues=venues)
-
-    @app.route('/predict', methods=['GET', 'POST'])
-    def predict():
-        if request.method == 'POST':
-            # Access user input from form
-            team1 = request.form['team1']
-            team2 = request.form['team2']
-            venue = request.form['venue']
-
-            # Encode user input using label encoder
-            team1_encoded = team_label_encoder.transform([team1])[0]
-            team2_encoded = team_label_encoder.transform([team2])[0]
-            venue_encoded = venue_label_encoder.transform([venue])[0]
-
-            # Modify the new_data to use team1 and team2 selected by the user
-            new_data = [[team1_encoded, team2_encoded, venue_encoded]]
-
-            # Make prediction
-            predicted_winner = model.predict(new_data)
-
-            # Decode the predicted winner label
-            decoded_winner = team_label_encoder.inverse_transform(predicted_winner)
-
-            # Return predicted winner to the template for rendering
-            return render_template('result.html', predicted_winner=decoded_winner[0])
-
-        # Render the form for user input
-        return render_template('form.html')
-   
-        @app.route('/tipme')
-        def goback():
-            return render_template('tipme.html')
-        pass
-
-# Register signal handler in main thread
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
-
+# Run the Flask app with SocketIO
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
     socketio.run(app)
