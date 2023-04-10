0d5d42e2c55703e7a686fdfdc91dff03f52e97c2
Ankit Raj <96245152+Ankiit04@users.noreply.github.com>
2023-04-10 07:18:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,89 @@
 import streamlit as st
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
-# Load the dataset
-ipl = pd.read_csv("path/to/your/ipl.csv")  # Replace with the path to your dataset
+# Load the IPL matches dataset
+ipl = pd.read_csv('matches.csv')
 
-# Add 'Rising Pune Supergiant' to the list of labels
-ipl['label'] = ipl['label'].append(pd.Series(['Rising Pune Supergiant']))
+# Define dictionary to rename team names
+team_rename_dict = {
+    'Gujarat Lions': 'Gujarat Titans',
+    'Kings XI Punjab': 'Punjab Kings',
+    'Rising Pune Supergiant': 'Lucknow Supergiants',
+    'Delhi Daredevils': 'Delhi Capitals',
+    # Add more team name mappings as needed
+}
 
-# Encode the labels
-label_encoder = LabelEncoder()
-ipl['label'] = label_encoder.fit_transform(ipl['label'])
+# Define dictionary to rename venue names
+venue_rename_dict = {
+    'ACA-VDCA Stadium': 'Arun Jaitley Stadium',
+    'Green Park': 'Narendra Modi Stadium',
+    'Buffalo Park': 'Ekana Cricket Stadium',
+    # Add more venue name mappings as needed
+}
+
+# Rename team names
+ipl['team1'] = ipl['team1'].replace(team_rename_dict)
+ipl['team2'] = ipl['team2'].replace(team_rename_dict)
+ipl['winner'] = ipl['winner'].replace(team_rename_dict)
+
+# Rename venue names
+ipl['venue'] = ipl['venue'].replace(venue_rename_dict)
+
+# Drop unnecessary columns
+ipl.drop(['dl_applied', 'toss_winner', 'toss_decision', 'result', 'win_by_runs', 'win_by_wickets', 'player_of_match', 'umpire1', 'umpire2', 'umpire3', 'date', 'city', 'season', 'id'], axis=1, inplace=True)
+
+# Drop rows with NaN values in the 'winner' column
+ipl.dropna(subset=['winner'], inplace=True)
+
+# Encode categorical variables
+team_label_encoder = LabelEncoder()
+venue_label_encoder = LabelEncoder()
+ipl['team1'] = team_label_encoder.fit_transform(ipl['team1'])
+ipl['team2'] = team_label_encoder.transform(ipl['team2'])
+ipl['winner'] = team_label_encoder.transform(ipl['winner'])
+ipl['venue'] = venue_label_encoder.fit_transform(ipl['venue'])
+
+teams_to_remove = ['Kochi Tuskers Kerala', 'Pune Warriors', 'Deccan Chargers']
+
+# Filter out matches involving teams to be removed
+ipl = ipl[~ipl['team1'].isin(teams_to_remove)]
+ipl = ipl[~ipl['team2'].isin(teams_to_remove)]
+ipl = ipl[~ipl['winner'].isin(teams_to_remove)]
+
+# Get unique team names and venue names
+teams = [team for team in team_label_encoder.inverse_transform(ipl['team1'].unique())]
+teams.remove(teams_to_remove[0])
+teams.remove(teams_to_remove[1])
+teams.remove(teams_to_remove[2])
+
+venues = venue_label_encoder.inverse_transform(ipl['venue'].unique())
 
 # Split the data into training and testing sets
-X = ipl.iloc[:, :-1]  # Features
-y = ipl.iloc[:, -1]  # Labels
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train = ipl[['team1', 'team2', 'venue']]
+y_train = ipl['winner']
 
-# Train a decision tree classifier
-clf = DecisionTreeClassifier()
-clf.fit(X_train, y_train)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
 
-# Create a Streamlit app
-def predict_label(features):
-    X_input = pd.DataFrame([features], columns=X.columns)
-    y_pred = clf.predict(X_input)
-    y_pred_decoded = label_encoder.inverse_transform(y_pred)
-    return y_pred_decoded[0]
+# Streamlit app
+st.title('IPL Match Winner Prediction')
+st.markdown('---')
 
-def main():
-    st.title("IPL Team Prediction")
-    st.write("Enter the values for the features")
+# Render the form for user input
+team1 = st.selectbox('Select Team 1', teams)
+team2 = st.selectbox('Select Team 2', teams)
+venue = st.selectbox('Select Venue', venues)
 
-    # Collect input from the user
-    feature1 = st.slider("Feature 1", min_value=0, max_value=10, value=5)
-    feature2 = st.slider("Feature 2", min_value=0, max_value=10, value=5)
-    feature3 = st.slider("Feature 3", min_value=0, max_value=10, value=5)
+if st.button('Predict'):
+    # Encode user input using label encoder
+    team1_encoded = team_label_encoder.transform([team1])[0]
+    team2_encoded = team_label
+# Make prediction
+prediction = model.predict([[team1_encoded, team2_encoded, venue_encoded]])
+predicted_winner = team_label_encoder.inverse_transform(prediction)[0]
 
-    # Predict on the input data
-    prediction = predict_label([feature1, feature2, feature3])
-
-    # Display the predicted label
-    st.write("Predicted Label:", prediction)
-
-if __name__ == "__main__":
-    main()
+# Render prediction result
+st.markdown('**Predicted Winner:** ' + predicted_winner)
